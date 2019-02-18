6e59dedce513e83cbc376fbebe13bd3770db5698
mrcsan <saranmarcin@gmail.com>
2019-02-18 11:44:59 +0100

    Minor change in settings.py

--- forumPyDj/urls.py
+++ forumPyDj/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('forum/', include('forum.urls')),
     path('admin/', admin.site.urls),
+    path('forum/', include('forum.urls')),
 ]
