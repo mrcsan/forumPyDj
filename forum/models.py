0ab276240a1b38712619e6c76c94be62652adfd1
mrcsan <saranmarcin@gmail.com>
2019-02-21 19:43:32 +0100

    User ForeignKey added to Thread and Post

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Thread(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    thread_text = models.CharField(max_length=500)
+    pub_date = models.DateTimeField('date published')
+
+
+class Post(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
