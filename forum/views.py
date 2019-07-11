b573194433bc5950e538628cfb178531e063bae6
mrcsan <saranmarcin@gmail.com>
2019-07-11 19:53:24 +0200

    Authentication check implementation for home page.

--- forum/views.py
+++ forum/views.py
@@ -6,16 +6,25 @@ from django.contrib.auth.forms import AuthenticationForm
 from forum.forms import SignUpForm
 from .models import Topic, Post
 
-
 def index(request):
-    
-    text_header = "Welcome in PyDJ Forum!"
-    text_paragraph = "Please log in or create an account. xD"
+    if request.user.is_authenticated:
+        text_header = "Dashboard"
+        text_paragraph = "This is content" 
+        topic_list = Topic.objects.order_by('-published_date')[:5]
+        post_list = Post.objects.order_by('-published_date')[:5]
+        #output = ', '.join([t.thread_title for t in latest_threads_list])
 
-    args = {'header': text_header, 
+        args = {'header': text_header, 'paragraph': text_paragraph, 'latest_topic_list': topic_list, 'latest_post_list': post_list}
+
+        return  render(request, 'forum/index.html', args)         
+    else:
+        text_header = "Welcome in PyDJ Forum!"
+        text_paragraph = "Please log in or create an account. xD"
+
+        args = {'header': text_header, 
             'paragraph': text_paragraph}
 
-    return render(request, 'forum/index.html', args)
+        return render(request, 'forum/index.html', args)
 
 def detail(request, topic_id):
     return HttpResponse("You're looking at thread %s." % topic_id)
@@ -30,7 +39,7 @@ def registrationView(request):
             print("email: "+str(form.cleaned_data.get('email')))
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('logedin')
+            return redirect('index')
     else:
         form = SignUpForm()
     return render(request, 'forum/registration.html', {'form': form})
