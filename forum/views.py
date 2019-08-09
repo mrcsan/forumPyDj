2baad05ed0e1a512e70b2f3036daa391f9aebaf1
mrcsan <saranmarcin@gmail.com>
2019-08-09 14:29:36 +0200

    Slight views refactor.

--- forum/views.py
+++ forum/views.py
@@ -9,14 +9,14 @@ from .models import Topic, Post
 def index(request):
     if request.user.is_authenticated:
         text_header = "Dashboard"
-        text_paragraph = "This is content" 
+        text_paragraph = "Latest topics:" 
         topic_list = Topic.objects.order_by('-published_date')[:5]
-        post_list = Post.objects.order_by('-published_date')[:5]
-        #output = ', '.join([t.thread_title for t in latest_threads_list])
 
-        args = {'header': text_header, 'paragraph': text_paragraph, 'latest_topic_list': topic_list, 'latest_post_list': post_list}
 
-        return  render(request, 'forum/index.html', args)         
+        args = {'header': text_header, 'paragraph': text_paragraph, 'latest_topic_list': topic_list}
+
+        #return render(request, 'forum/index.html', args)
+         
     else:
         text_header = "Welcome in PyDJ Forum!"
         text_paragraph = "Please log in or create an account. xD"
@@ -24,17 +24,26 @@ def index(request):
         args = {'header': text_header, 
             'paragraph': text_paragraph}
 
-        return render(request, 'forum/index.html', args)
+    return render(request, 'forum/index.html', args)
 
 def threadView(request, thread_id):
+    if request.user.is_authenticated:
+        thread_title = thread_id
+        post_list = Post.objects.order_by('-published_date')[:5]
 
-    text_header = "Ups!"
-    text_paragraph = "It seems that you are not logged in. If you want to see this page please authenticate. ;)"
+        args = {'header': thread_title, 'paragraph': text_paragraph, 'latest_post_list': post_list}
 
-    return HttpResponse("You're looking at thread %s." % thread_id)
+    else:   
+        text_header = "Ups!"
+        text_paragraph = "It seems that you are not logged in. If you want to see this page please authenticate. ;)"
+
+        args = {'header': text_header, 
+        'paragraph': text_paragraph}
+
+    return render(request, 'forum/thread.html', args)
 
 def allThreadsView(request, thread_id):
-    return HttpResponse("You're looking at thread %s." % thread_id)
+    return 
 
 #def allPostsView(request, thread_id):
     #return HttpResponse("You're looking at thread %s." % thread_id)
